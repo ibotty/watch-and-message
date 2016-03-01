2f16a9b9e48fe66597ac400d17ba31f48a1c0bf1
Tobias Florek <tob@butter.sh>
2016-03-01 15:15:54 +0100

    rename across filesystem boundaries

--- src/System/Watcher/MoveFiles.hs
+++ src/System/Watcher/MoveFiles.hs
@@ -7,29 +7,39 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Foldable (traverse_)
 import Filesystem.Path.CurrentOS (encodeString)
 import GHC.IO.Exception (IOErrorType(UnsupportedOperation))
-import System.Directory (createDirectory, copyFile, removeFile, getDirectoryContents, renameFile)
+import System.Directory (createDirectory, copyFile, renameDirectory
+                        , removeFile , getDirectoryContents, renameFile)
 import System.FilePath ((</>), replaceDirectory)
 import System.IO.Error (catchIOError, ioeGetErrorType)
 
 import qualified Turtle
 
 moveDirectory :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ()
-moveDirectory src dst = liftIO $ do
-    catchIOError (createDirectory dst') (const $ return ()) -- ignore any error
-    traverse_ (flip moveToDir dst') =<< map (src' </>). filter f <$> getDirectoryContents src'
+moveDirectory src dst = liftIO $
+    renameDirectory src' dst' `catchUnsupported` renameDirectory' src' dst'
   where
-    f n = n /= "." && n /= ".."
     src' = encodeString src
     dst' = encodeString dst
 
+renameDirectory' :: FilePath -> FilePath -> IO ()
+renameDirectory' src' dst' = do
+    createDirectory dst' `catchUnsupported` return ()
+    files <- map (src' </>). filter f <$> getDirectoryContents src'
+    traverse_ (flip moveToDir dst') files
+  where
+    f n = n /= "." && n /= ".."
+
+catchUnsupported :: IO a -> IO a -> IO a
+catchUnsupported action ifFailedAction =
+    catchIOError action handler
+  where
+    handler (ioeGetErrorType -> UnsupportedOperation) = ifFailedAction
+    handler ioe                                       = ioError ioe
+
 moveToDir :: FilePath -> FilePath -> IO ()
 moveToDir src dst =
-    catchIOError (renameFile src dstFile)
-                  handler
-
-  where
-    dstFile = replaceDirectory src dst
-    handler (ioeGetErrorType -> UnsupportedOperation) = do
+    catchUnsupported (renameFile src dstFile) $ do
         copyFile src dstFile
         removeFile src
-    handler ioe = ioError ioe
+  where
+    dstFile = replaceDirectory src dst
