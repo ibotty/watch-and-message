00899fe0bfc9a791434182432bf5cafd5d526bdd
Tobias Florek <tob@butter.sh>
2016-03-01 13:33:13 +0100

    fix moveToDir

--- src/System/Watcher/MoveFiles.hs
+++ src/System/Watcher/MoveFiles.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ViewPatterns #-}
 module System.Watcher.MoveFiles
   ( moveDirectory
   ) where
@@ -5,15 +6,16 @@ module System.Watcher.MoveFiles
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Foldable (traverse_)
 import Filesystem.Path.CurrentOS (encodeString)
-import System.Directory (createDirectory, getDirectoryContents, renameFile)
+import GHC.IO.Exception (IOErrorType(UnsupportedOperation))
+import System.Directory (createDirectory, copyFile, removeFile, getDirectoryContents, renameFile)
 import System.FilePath ((</>), replaceDirectory)
-import System.IO.Error (catchIOError)
+import System.IO.Error (catchIOError, ioeGetErrorType)
 
 import qualified Turtle
 
 moveDirectory :: MonadIO io => Turtle.FilePath -> Turtle.FilePath -> io ()
 moveDirectory src dst = liftIO $ do
-    catchIOError (createDirectory dst') (const $ return ()) -- ignore error
+    catchIOError (createDirectory dst') (const $ return ()) -- ignore any error
     traverse_ (flip moveToDir dst') =<< map (src' </>). filter f <$> getDirectoryContents src'
   where
     f n = n /= "." && n /= ".."
@@ -21,4 +23,13 @@ moveDirectory src dst = liftIO $ do
     dst' = encodeString dst
 
 moveToDir :: FilePath -> FilePath -> IO ()
-moveToDir src dst = renameFile src (replaceDirectory src dst)
+moveToDir src dst =
+    catchIOError (renameFile src dstFile)
+                  handler
+
+  where
+    dstFile = replaceDirectory src dst
+    handler (ioeGetErrorType -> UnsupportedOperation) = do
+        copyFile src dstFile
+        removeFile src
+    handler ioe = ioError ioe
